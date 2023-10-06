67596d22e615563a1ae39da9f8c9c53e0d8cfd39
Preeyanuch4748 <preeyanuchhomseang@gmail.com>
2023-10-06 17:34:05 +0700

    first commit

---
+++ src/pages/InputPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="htmlClass">
+      <h1>{{ title }}</h1>
+      <form>
+        <input type="text" id="firstName" v-model="firstName" />
+        <input type="text" id="secondName" v-model="secondName" />
+        <button @click="submitForm()" class="submitForm">Submit</button>
+    </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'InputPage',
+    data() {
+      return {
+        title: 'Preeyanuch Homseang',
+        firstName: '',
+        secondName: '',
+      }
+    },
+    methods: {
+      submitForm() {
+        this.$emit('submit form')
+      },
+    }
+  }
+  </script>
